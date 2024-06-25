1038f1da932d8b3eee8ed0b0e637856c53342c00
pedro-makoski <148820090+pedro-makoski@users.noreply.github.com>
2024-06-25 14:29:14 -0300

    Script para formatação a partir de pattern
    Só para constar, esse script possui esse problema:
    - Não lidar com caracteres, apenas com números.

---
+++ exercicios/ex020/script.js
@@ -0,0 +1,87 @@
+const inputs = Array.from(document.querySelectorAll('input[pattern^="^"]'))
+
+function to_arr(pattern) {
+    const pattern_compacted = []
+    let count = 0
+    let tamanho_digito_expressao = '[0-9]'.length
+
+
+    for(let i = 1; i < pattern.length - 1; i++) {
+        if(pattern[i] !== '\\' && (pattern[i-1] !== '{' && pattern[i+1] !== '}' && pattern[i] !== '{' && pattern[i] !== '}') && pattern[i] !== 'd') {
+            pattern_compacted[count] = pattern[i]
+            count ++
+        } else if(pattern[i-1] === '{') {
+            let number = 0
+            for(let j = i; j < pattern.length; j++) {
+                if(pattern[j-1] === '}') {
+                    number = parseInt(pattern.slice(i, j))  
+                    j = pattern.length
+                }
+            }
+
+            if(pattern[i-2] === 'd') {
+                for(let j = count; j < count + number; j++) {
+                    pattern_compacted[j] = 'number'
+                }
+            }
+
+            if (pattern.slice(i-(tamanho_digito_expressao)-1, i-1) === '[0-9]') {  
+                count -= tamanho_digito_expressao
+               
+                for(let j = count; j < count + number; j++) {
+                    pattern_compacted[j] = 'number'
+                }
+
+                if(number < tamanho_digito_expressao) {
+                    for(let j = count + number; j < count + number + (tamanho_digito_expressao - number); j++) {
+                        pattern_compacted.pop(j)
+                    } 
+                }
+            }
+
+            count += number
+        }
+    }
+
+    return pattern_compacted
+}
+
+function to_string(arr, value) {
+    console.log(arr)
+    let new_value = Array.from(value)
+    let finish_for = false 
+
+    if(new_value.length >= arr.length) {
+        new_value = new_value.slice(0, arr.length)
+    }
+
+    for(let i = 0; !finish_for; i++) {
+        if(new_value[i] !== arr[i] && isNaN(parseInt(new_value[i]))){
+            new_value.splice(i, 1)
+        }else if(new_value[i] !== arr[i] && arr[i] !== 'number' && !isNaN(parseInt(new_value[i]))) {
+            new_value.splice(i, 0, arr[i])
+        }
+
+        if(i > new_value.length) {
+            console.log(i, new_value.length, value.length)
+            finish_for = true
+        }   
+    }
+
+    return new_value.join('')
+}
+
+inputs.map((input) => {
+    input.addEventListener('input', () => {
+        let valor = input.value
+        //let valor = '4199690-4836'
+        let pattern = input.getAttribute('pattern')
+
+        let pattern_arr = to_arr(pattern)
+        let new_value = to_string(pattern_arr, valor)
+
+        input.value = new_value
+
+        console.log(new_value)
+    })
+})
